540add448efe52bf5dbc17ba835f2bf4324ada79
t0int <56300981+t0int@users.noreply.github.com>
2022-04-26 22:52:11 +0300

    Update logger.py
    added docstrings

--- logger.py
+++ logger.py
@@ -1,20 +1,43 @@
-# Coded by t0int <3 using colorama
 import colorama
 
-class logger:
+class Logger:
     def __init__(self, log_file):
-        colorama.init()
         self.log_file = log_file
         self.log_file_handle = open(log_file, 'w')
-    def error(self,str):
-        print(colorama.Fore.RED + "[ERROR]: "+str + colorama.Fore.WHITE)
-        self.log_file_handle.write("[ERROR] " + str + "\n")
-    def warning(self,str):
-        print(colorama.Fore.YELLOW + str + colorama.Fore.WHITE)
-        self.log_file_handle.write("[WARNING] " + str + "\n")
-    def info(self,str):
-        print(colorama.Fore.GREEN + str + colorama.Fore.WHITE)
-        self.log_file_handle.write("[INFO] " + str + "\n")
-    def debug(self,str):
-        print(colorama.Fore.BLUE + str + colorama.Fore.WHITE)
-        self.log_file_handle.write("[DEBUG] " + str + "\n")
+        colorama.init()
+    def error(self,log_str):
+        """Log an error message to log file and console
+
+        Args:
+            log_str (str): String to log to file and console
+        """
+        
+        print(colorama.Fore.RED + "[ERROR]: "+log_str + colorama.Fore.WHITE)
+        self.log_file_handle.write("[ERROR] " + log_str + "\n")
+    def warning(self,log_str):
+        """Log an warning message to log file and console
+
+        Args:
+            log_str (str): String to log to file and console
+        """
+        
+        print(colorama.Fore.YELLOW + log_str + colorama.Fore.WHITE)
+        self.log_file_handle.write("[WARNING] " + log_str + "\n")
+    def info(self,log_str):
+        """Log an information message to log file and console
+
+        Args:
+            log_str (str): String to log to file and console
+        """
+        
+        print(colorama.Fore.GREEN + log_str + colorama.Fore.WHITE)
+        self.log_file_handle.write("[INFO] " + log_str + "\n")
+    def debug(self,log_str):
+        """Log an debugging message to log file and console
+
+        Args:
+            log_str (str): String to log to file and console
+        """
+        
+        print(colorama.Fore.BLUE + log_str + colorama.Fore.WHITE)
+        self.log_file_handle.write("[DEBUG] " + log_str + "\n")
