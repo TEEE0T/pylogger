ced7a9a3b73d89e3413e943eef1d2946f6e47445
t0int <56300981+t0int@users.noreply.github.com>
2022-04-26 23:00:29 +0300

    Update logger.py
    fixed some skill issues

--- logger.py
+++ logger.py
@@ -1,9 +1,13 @@
-import colorama
+import colorama # For color in terminal
+import io
+import datetime
 
 class Logger:
+    """Logging class for logging messages to file and console"""
     def __init__(self, log_file):
         self.log_file = log_file
-        self.log_file_handle = open(log_file, 'w')
+        self.log_file_handle = io.open(log_file, "w", encoding="utf-8")
+        self.log_file_handle.write("Log file created at " + str(datetime.now()) + "\n")
         colorama.init()
     def error(self,log_str):
         """Log an error message to log file and console
@@ -11,33 +15,29 @@ class Logger:
         Args:
             log_str (str): String to log to file and console
         """
-        
-        print(colorama.Fore.RED + "[ERROR]: "+log_str + colorama.Fore.WHITE)
-        self.log_file_handle.write("[ERROR] " + log_str + "\n")
+        print(colorama.Fore.RED + "[" + datetime.now() + "] [ERROR]: "+log_str + colorama.Fore.WHITE)
+        self.log_file_handle.write("[" + datetime.now() + "] [ERROR] " + log_str + "\n")
     def warning(self,log_str):
         """Log an warning message to log file and console
 
         Args:
             log_str (str): String to log to file and console
         """
-        
-        print(colorama.Fore.YELLOW + log_str + colorama.Fore.WHITE)
-        self.log_file_handle.write("[WARNING] " + log_str + "\n")
+        print(colorama.Fore.YELLOW + "[" + datetime.now() + "] [WARNING] " + log_str + colorama.Fore.WHITE)
+        self.log_file_handle.write("[" + datetime.now() + "] [WARNING] " + log_str + "\n")
     def info(self,log_str):
         """Log an information message to log file and console
 
         Args:
             log_str (str): String to log to file and console
         """
-        
-        print(colorama.Fore.GREEN + log_str + colorama.Fore.WHITE)
-        self.log_file_handle.write("[INFO] " + log_str + "\n")
+        print(colorama.Fore.GREEN + "[" + datetime.now() + "] [INFO] " + log_str + colorama.Fore.WHITE)
+        self.log_file_handle.write("[" + datetime.now() + "] [INFO] " + log_str + "\n")
     def debug(self,log_str):
         """Log an debugging message to log file and console
 
         Args:
             log_str (str): String to log to file and console
         """
-        
-        print(colorama.Fore.BLUE + log_str + colorama.Fore.WHITE)
-        self.log_file_handle.write("[DEBUG] " + log_str + "\n")
+        print(colorama.Fore.BLUE + "[" + datetime.now() + "] [DEBUG] " + log_str + colorama.Fore.WHITE)
+        self.log_file_handle.write("[" + datetime.now() + "] [DEBUG] " + log_str + "\n")
